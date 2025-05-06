0a820ef8ed8d379d71f3fbf658570445778423c2
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-05-06 19:04:30 +0100

    Extend abstraction via creation of `IHealthCheckConnection` interface

---
+++ src/Riverside.Hypervisor/Connections/IHealthCheckConnection.cs
@@ -0,0 +1,14 @@
+﻿namespace Riverside.Hypervisor.Connections;
+
+/// <summary>
+/// Extends IConnection to support health monitoring.
+/// Implementations can use this interface to provide keep-alive and monitoring features.
+/// </summary>
+public interface IHealthCheckConnection : IConnection
+{
+	/// <summary>
+	/// Asynchronously performs a health check on the connection.
+	/// </summary>
+	/// <returns><c>true</c> if the connection is healthy; otherwise, <c>false</c>.</returns>
+	Task<bool> CheckHealthAsync();
+}
